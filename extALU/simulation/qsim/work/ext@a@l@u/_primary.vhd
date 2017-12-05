a5d406cdcdf47bce753e179722fb421d8fcf7e8d
Ben Shen <shenleyang007@gmail.com>
2017-12-05 14:53:08 +0000

    add ASL to ALU, independent of SUB

--- extALU/simulation/qsim/work/ext@a@l@u/_primary.vhd
+++ extALU/simulation/qsim/work/ext@a@l@u/_primary.vhd
@@ -4,6 +4,7 @@ entity extALU is
     port(
         Carry_out       : out    vl_logic;
         SUB             : in     vl_logic;
+        ASL             : in     vl_logic;
         A               : in     vl_logic_vector(3 downto 0);
         B               : in     vl_logic_vector(3 downto 0);
         B_is_1          : in     vl_logic;
